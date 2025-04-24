f5b0526c2c49bb2d64bd3f2c110058c05b19d775
freetimezp <kraftukrnet@ukr.net>
2025-04-24 22:24:40 +0300

    03. usefull-things-138-search. Search.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,49 @@
 
+const searchList = () => {
+    let input, filter, ul, li, a, txtValue;
+    let visibleCount = 0;
+
+    input = document.getElementById("search-input");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("search-items");
+    li = ul.getElementsByTagName("li");
+
+    for (let i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+            visibleCount++;
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+
+    document.querySelector(".search-item-container p span").textContent = visibleCount;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
